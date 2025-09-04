271b723bee8503b9ed3e0f4a59003c2ae5f6bbc5
DevidarDhruvil <Dhruvil.Devidar@technovate.in>
2025-09-04 12:08:58 +0530

    Backend Setup along with database connection

---
+++ server/server.js
@@ -0,0 +1,23 @@
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import connectDB from "./configs/db.js";
+
+// Initialize Express App
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Connect to Database
+await connectDB();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Basic Route
+app.get("/", (req, res) => res.send("Server is running..."));
+
+// Start the server
+app.listen(PORT, () =>
+  console.log(`Server is running on http://localhost:${PORT}`)
+);
