6bdc988a761664b1e4d44483d7e9d4fc611e2347
DevidarDhruvil <Dhruvil.Devidar@technovate.in>
2025-09-04 15:44:18 +0530

    Added the auth to check before getting the userdata which is also added in this commit

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,9 +1,15 @@
 import express from "express";
-import { loginUser, registerUser } from "../controllers/usercontroller.js";
+import {
+  getUserData,
+  loginUser,
+  registerUser,
+} from "../controllers/usercontroller.js";
+import protect from "../middleware/auth.js";
 
 const userRouter = express.Router();
 
 userRouter.post("/register", registerUser);
 userRouter.post("/login", loginUser);
+userRouter.get("/data", protect, getUserData);
 
 export default userRouter;
