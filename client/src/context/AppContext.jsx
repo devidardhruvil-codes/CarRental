5a0f0d299948fd91c180750c19478d301d3eb2b4
DevidarDhruvil <Dhruvil.Devidar@technovate.in>
2025-09-08 12:11:54 +0530

    Toaster addition and API call functions are added

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,9 +1,95 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
+import axios from "axios";
+import { toast } from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
+
+axios.defaults.baseURL = import.meta.env.VITE_BASE_URL;
 
 export const AppContext = createContext();
 
 export const AppProvider = ({ children }) => {
-  const value = {};
+  const navigate = useNavigate();
+  const currency = import.meta.env.VITE_CURRENCY;
+
+  const [token, setToken] = useState(null);
+  const [user, setUser] = useState(null);
+  const [owner, setOwner] = useState(false);
+  const [showLogin, setShowLogin] = useState(false);
+  const [pickupDate, setPickupDate] = useState("");
+  const [returnDate, setReturnDate] = useState("");
+  const [cars, setCars] = useState([]);
+
+  const value = {
+    navigate,
+    currency,
+    token,
+    setToken,
+    user,
+    setUser,
+    owner,
+    setOwner,
+    showLogin,
+    setShowLogin,
+    pickupDate,
+    setPickupDate,
+    returnDate,
+    setReturnDate,
+    cars,
+    setCars,
+    fetchCars,
+    fetchUser,
+    logout,
+  };
+
+  // UseEffect to fetch token from localstorage
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    setToken(token);
+    fetchCars();
+  }, []);
+
+  // UseEffect to fetch user data when token is available
+  useEffect(() => {
+    if (token) {
+      axios.defaults.headers.common["Authorization"] = `${token}`;
+      fetchUser();
+    }
+  }, [token]);
+
+  // Function to check if user is logged in
+  const fetchUser = async () => {
+    try {
+      const { data } = await axios.get("/api/user/data");
+      if (data.success) {
+        setUser(data.user);
+        setOwner(data.user.role === "owner" ? true : false);
+      } else {
+        navigate("/");
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
+
+  // Function to fetch all cars
+  const fetchCars = async () => {
+    try {
+      const { data } = await axios.get("/api/user/cars");
+      data.success ? setCars(data.cars) : toast.error(data.message);
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
+
+  // Function to logout user
+  const logout = () => {
+    localStorage.removeItem("token");
+    setToken(null);
+    setUser(null);
+    setOwner(false);
+    axios.defaults.headers.common["Authorization"] = "";
+    toast.success("Logged out successfully");
+  };
 
   return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
 };
