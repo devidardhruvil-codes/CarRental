7bc8bf0e8696a4a03f2230b41ae22ea76e125759
DevidarDhruvil <Dhruvil.Devidar@technovate.in>
2025-09-08 17:28:49 +0530

    UI: Dashboard Data comes from API now
    API: Dashboard API changed from post to get method

--- client/src/pages/owner/Layout.jsx
+++ client/src/pages/owner/Layout.jsx
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Sidebar from "../../components/owner/Sidebar";
 import { Outlet } from "react-router-dom";
 import NavbarOwner from "../../components/owner/NavbarOwner";
+import { useAppContext } from "../../context/AppContext";
 
 const Layout = () => {
+  const { owner, navigate } = useAppContext();
+
+  useEffect(() => {
+    if (!owner) {
+      navigate("/");
+    }
+  }, [owner]);
   return (
     <div className="flex flex-col">
       <NavbarOwner />
--- server/routes/ownerRoutes.js
+++ server/routes/ownerRoutes.js
@@ -19,7 +19,7 @@ ownerRouter.get("/cars", protect, getOwnerCars);
 ownerRouter.post("/toggle-car", protect, toggleCarAvailability);
 ownerRouter.post("/delete-car", protect, deleteCar);
 
-ownerRouter.post("/dashboard", protect, getDashboardData);
+ownerRouter.get("/dashboard", protect, getDashboardData);
 ownerRouter.post(
   "/update-image",
   upload.single("image"),
