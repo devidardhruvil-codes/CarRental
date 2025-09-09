eea6f7e0dc1c2190190215508c7cefc5b7b69383
DevidarDhruvil <Dhruvil.Devidar@technovate.in>
2025-09-09 14:52:51 +0530

    UI: In dashboard, ManageBookings page is updated to call API for to fetch the Bookings of the owner and the also to change the booking status for that car.

--- client/src/pages/owner/ManageBookings.jsx
+++ client/src/pages/owner/ManageBookings.jsx
@@ -1,13 +1,36 @@
-import React, { useEffect, useState } from "react";
-import { dummyMyBookingsData } from "../../assets/assets";
+import { useEffect, useState } from "react";
 import Title from "../../components/owner/Title";
+import { useAppContext } from "../../context/AppContext";
+import toast from "react-hot-toast";
 
 const ManageBookings = () => {
-  const currency = import.meta.env.VITE_CURRENCY;
+  const { currency, axios } = useAppContext();
   const [bookings, setBookings] = useState([]);
 
   const fetchOwnerBookings = async () => {
-    setBookings(dummyMyBookingsData);
+    try {
+      const { data } = await axios.get("/api/bookings/owner");
+      data.success ? setBookings(data.bookings) : toast.error(data.error);
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
+
+  const changeBookingStatus = async ({ bookingId, status }) => {
+    try {
+      const { data } = await axios.post("/api/bookings/change-status", {
+        bookingId,
+        status,
+      });
+      if (data.success) {
+        toast.success(data.success);
+        fetchOwnerBookings();
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
   };
 
   useEffect(() => {
@@ -67,6 +90,9 @@ const ManageBookings = () => {
                 <td className="p-3">
                   {booking.status === "pending" ? (
                     <select
+                      onChange={(e) =>
+                        changeBookingStatus(booking._id, e.target.value)
+                      }
                       value={booking.status}
                       className="px-2 py-1.5 mt-1 text-gray-500 border border-borderColor rounded-md outline-none"
                     >
